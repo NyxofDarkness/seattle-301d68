05b96b0f31dff806c649da995f9715a7ec6b66ce
NyxOfDarkness <ibelieve.as@gmail.com>
2020-10-31 22:58:54 -0700

    second arrow function done

--- lab-b/starter-code/app.js
+++ lab-b/starter-code/app.js
@@ -116,14 +116,18 @@ const sum = (a, b, c, d) => `${a + b + c + d}`
 console.log(sum(1, 2, 3, 4));
 
 
-let objectLit = function () {
-  return {
-    key1: 'value1',
-    key2: 'value2',
-    key3: 'value3',
-  };
-};
-
+// let objectLit = function () {
+//   return {
+//     key1: 'value1',
+//     key2: 'value2',
+//     key3: 'value3',
+//   };
+// };
+const objectLit = array => ({
+  key1: 'value1',
+  key2: 'value2',
+  key3: 'value3',
+});
 // TODO: Uncomment the following line of code to see the output in the browser console
 console.log(objectLit());
 
